4765c53285c69c1db21de5f41dd6728838f737cd
Matija Petrunic <matija.petrunic@fer.hr>
2020-09-15 12:24:00 +0200

    Remove config files

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,41 +6,12 @@ import (
 	"time"
 
 	"github.com/getsentry/sentry-go"
-	"github.com/spf13/viper"
-	"github.com/subosito/gotenv"
 )
 
 func InitMainConfig() {
-	setDefaultValuesForMainConfig()
-	viper.SetConfigName(getMainConfigName()) // name of config file (without extension)
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	_ = viper.ReadInConfig()
-
-	// Load env variables from .env
-	var err = gotenv.Load()
-	if err != nil {
-		fmt.Println("Gotenv load failed")
-	}
-
 	if os.Getenv("ENV") != "test" {
 		setupSentry()
 	}
-}
-
-func setDefaultValuesForMainConfig() {
-	viper.SetDefault("stats.interval", 30)
-	viper.SetDefault("log.level", "error")
-}
-
-// depending on ENV variable creates name for config file
-func getMainConfigName() string {
-	configFileName := "config"
-	if env := os.Getenv("ENV"); env != "" {
-		configFileName = configFileName + "-" + env
-	}
-
-	return configFileName
 }
 
 func setupSentry() {
