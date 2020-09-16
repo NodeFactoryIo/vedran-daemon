48c5552fff27e3aa78fb5bd59a43d725b86b7c6a
Matija Petrunic <matija.petrunic@fer.hr>
2020-09-16 12:47:14 +0200

    Register command logic

--- cmd/register.go
+++ cmd/register.go
@@ -1,18 +1,22 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
+	"log"
+	"net/url"
+	"time"
 
+	"github.com/NodeFactoryIo/vedran-daemon/internal/lb"
+	"github.com/NodeFactoryIo/vedran-daemon/internal/metrics"
+	"github.com/go-co-op/gocron"
 	"github.com/spf13/cobra"
 )
 
 var (
-	node          string
+	nodeRPC       string
+	nodeMetrics   string
 	id            string
-	lb            string
+	lbBaseURL     string
 	payoutAddress string
 )
 
@@ -23,12 +27,14 @@ var registerCmd = &cobra.Command{
 }
 
 func init() {
-	registerCmd.Flags().StringVar(&node, "node", "", "Polkadot node url (required)")
+	registerCmd.Flags().StringVar(&nodeRPC, "node-rpc", "", "Polkadot node rpc url (required)")
+	registerCmd.Flags().StringVar(&nodeMetrics, "node-metrics", "", "Polkadot node metrics url (required)")
 	registerCmd.Flags().StringVar(&id, "id", "", "Vedran-daemon id string (required)")
-	registerCmd.Flags().StringVar(&lb, "lb", "", "Target load balancer url (required)")
+	registerCmd.Flags().StringVar(&lbBaseURL, "lb", "", "Target load balancer url (required)")
 	registerCmd.Flags().StringVar(&payoutAddress, "payout-address", "", "Payout address to which reward tokens will be sent (required)")
 
-	_ = registerCmd.MarkFlagRequired("node")
+	_ = registerCmd.MarkFlagRequired("node-rpc")
+	_ = registerCmd.MarkFlagRequired("node-metrics")
 	_ = registerCmd.MarkFlagRequired("id")
 	_ = registerCmd.MarkFlagRequired("lb")
 	_ = registerCmd.MarkFlagRequired("payout-address")
@@ -37,22 +43,33 @@ func init() {
 }
 
 func register(_ *cobra.Command, _ []string) error {
-	resp, err := http.Get(node + "/metrics")
-	if err != nil && resp.StatusCode != 200 {
-		return fmt.Errorf("Error polling polkadot node: %v", err)
-	}
-
-	values := map[string]string{
-		"id":             id,
-		"config_hash":    "config_hash",
-		"node_url":       node,
-		"payout_address": payoutAddress}
-	jsonValue, _ := json.Marshal(values)
-	resp, err = http.Post(lb+"/api/v1/nodes", "application/json", bytes.NewBuffer(jsonValue))
+	lbBaseURL, err := url.Parse(lbBaseURL)
 	if err != nil {
-		return fmt.Errorf("Error retrieving load balancer token: %v", err)
+		return fmt.Errorf("Failed parsing load balancer url")
 	}
 
-	fmt.Printf("Load balancer response: %v", resp)
+	client := lb.NewClient(lbBaseURL)
+	err = client.Register(id, nodeRPC, payoutAddress, "test-config-hash")
+	if err == nil {
+		return fmt.Errorf("Failed registering to load balancer on url %s because of: %v", lbBaseURL.String(), err)
+	}
+	log.Printf("Registered to load balancer %s", lbBaseURL)
+
+	scheduler := gocron.NewScheduler(time.UTC)
+
+	fms := &metrics.FetchMetricsService{BaseURL: nodeMetrics}
+	_, err = scheduler.Every(30).Seconds().Do(client.Metrics.Send, fms)
+	if err != nil {
+		return fmt.Errorf("Failed starting sending metrics becuase of: %v", err)
+	}
+	log.Println("Started sending metrics to load balancer")
+
+	_, err = scheduler.Every(5).Seconds().Do(client.Ping.Send)
+	if err != nil {
+		return fmt.Errorf("Failed starting sending pings becuase of: %v", err)
+	}
+	log.Println("Started sending pings to load balancer")
+
+	scheduler.StartBlocking()
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/NodeFactoryIo/vedran-daemon
 go 1.15
 
 require (
-	github.com/eugenmayer/go-exec v1.0.0
 	github.com/getsentry/sentry-go v0.7.0
+	github.com/go-co-op/gocron v0.3.1
 	github.com/prometheus/common v0.13.0
 	github.com/spf13/cobra v1.0.0
 	github.com/stretchr/testify v1.6.1
