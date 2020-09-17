d022bbde2f1a93a3a78be4cf83c027d0fc5b9a31
Matija Petrunic <matija.petrunic@fer.hr>
2020-09-17 09:50:57 +0200

    Remove extra method in interface

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -4,7 +4,6 @@ import "github.com/go-co-op/gocron"
 
 type Scheduler interface {
 	Every(interval uint64) *gocron.Scheduler
-	Seconds() *gocron.Scheduler
 	Do(jobFun interface{}, params ...interface{}) (*gocron.Job, error)
 	StartBlocking()
 }
