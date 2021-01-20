c9159ba963b227433d0d28cd6e85f3f493600d63
Mak Muftic <mak@nodefactory.io>
2021-01-20 13:24:46 +0100

    Expand metrics with target block

--- internal/node/metrics.go
+++ internal/node/metrics.go
@@ -16,6 +16,7 @@ type Metrics struct {
 	PeerCount             *float64 `json:"peer_count"`
 	BestBlockHeight       *float64 `json:"best_block_height"`
 	FinalizedBlockHeight  *float64 `json:"finalized_block_height"`
+	TargetBlockHeight     *float64 `json:"target_block_height"`
 	ReadyTransactionCount *float64 `json:"ready_transaction_count"`
 }
 
@@ -36,10 +37,20 @@ func (client *client) GetMetrics() (*Metrics, error) {
 		return nil, err
 	}
 
+	blockMetrics := metricFamilies["polkadot_block_height"].GetMetric()
+	bestBlockHeight := blockMetrics[0].Gauge.Value
+	finalizedBlockHeight := blockMetrics[1].Gauge.Value
+	targetBlockHeight := bestBlockHeight
+	// if node not synced there is additional metric - target block
+	if len(blockMetrics) == 3 {
+		targetBlockHeight = blockMetrics[2].Gauge.Value
+	}
+
 	metrics := &Metrics{
 		metricFamilies["polkadot_sync_peers"].GetMetric()[0].Gauge.Value,
-		metricFamilies["polkadot_block_height"].GetMetric()[0].Gauge.Value,
-		metricFamilies["polkadot_block_height"].GetMetric()[1].Gauge.Value,
+		bestBlockHeight,
+		finalizedBlockHeight,
+		targetBlockHeight,
 		metricFamilies["polkadot_ready_transactions_number"].GetMetric()[0].Gauge.Value,
 	}
 	return metrics, nil
